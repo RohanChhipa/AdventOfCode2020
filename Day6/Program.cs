b41870948117a1f8e0e9381f334cfc8606925bee
Rohan <rohan.chhipa@live.com>
2020-12-06 16:55:28 +0200

    Add Day 6

---
+++ Day6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using static System.Console;
+
+var input = ReadInput();
+
+var stopwatch = Stopwatch.StartNew();
+PartOne(input);
+PartTwo(input);
+WriteLine($"Time: {stopwatch.ElapsedMilliseconds}ms");
+
+string ReadInput()
+{
+    string[] lines = File.ReadAllLines("./input.txt");
+    return string.Join("\n", lines);
+}
+
+void PartOne(string input)
+{
+    var count = input.Split("\n\n")
+        .SelectMany(s => s.Replace("\n", "")
+            .ToCharArray()
+            .Distinct())
+        .Count();
+
+    WriteLine(count);
+}
+
+void PartTwo(string input)
+{
+    var groups = input.Split("\n\n");
+    var groupSize = groups
+        .Select(s => s.Count(x => x == '\n') + 1)
+        .ToArray();
+
+    var count = groups
+        .SelectMany((s, i) => s.Replace("\n", "")
+            .ToCharArray()
+            .GroupBy(c => c)
+            .Where(g => g.Count() == groupSize[i]))
+        .Count();
+
+    WriteLine(count);
+}
