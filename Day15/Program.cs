e2d360093d8ce284303ac6a1a163d52d3c7791f9
Rohan Chhipa <rohan.chhipa@entelect.co.za>
2020-12-17 10:12:45 +0200

    Add Day 15

---
+++ Day15/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using static System.Console;
+
+var input = File.ReadAllText("input.txt")
+    .Trim()
+    .Split(",")
+    .Select(int.Parse)
+    .ToList();
+
+WriteLine(GetLastNumber(2020));
+WriteLine(GetLastNumber(30000000));
+
+int GetLastNumber(int n)
+{
+    var last = input.Last();
+    var dictionary = new Dictionary<int, int>();
+    for (var k = 0; k < input.Count - 1; k++)
+        dictionary.Add(input[k], k + 1);
+
+    for (var k = input.Count; k < n; k++)
+    {
+        var value = 0;
+        if (dictionary.ContainsKey(last))
+        {
+            value = k - dictionary[last];
+            dictionary[last] = k;
+        }
+        else
+            dictionary.Add(last, k);
+
+        last = value;
+    }
+
+    return last;
+}
